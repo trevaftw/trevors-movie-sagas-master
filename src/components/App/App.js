dd0da1eafa1dd15d9eb22f29f004f85cf77ad85e
Trevor Ramlow <trevorramlow@gmail.com>
2019-06-22 10:24:35 -0500

    display the movie poster, title and description on the dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 class App extends Component {
   // Renders the entire app on the DOM
 
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
 
-  getMovies = () =>{
-    this.props.dispatch({type: 'FETCH_MOVIES'})
+  getMovies = () => {
+    this.props.dispatch({ type: 'FETCH_MOVIES' })
   }
 
   render() {
@@ -20,9 +20,29 @@ class App extends Component {
         <h1>HAVE THE MOVIE FETCH SETUP!</h1>
         <h2>See the console log to see the movies</h2>
         <p>Next get the images to display on the dom with a map. Create a component to do this. </p>
+        <br />
+        {/* {JSON.stringify(this.props.reduxState.movies, null, 2)} */}
+        <br />
+        <ul>
+          {this.props.reduxState.movies.map(movie => {
+            return (
+              <>
+
+                <img src={movie.poster} alt={movie.description} />
+                <li>{movie.title}</li><br />
+                <li>{movie.description}</li>
+              </>
+            )
+
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect()(App);
+const store = (reduxState) => ({
+  reduxState
+})
+
+export default connect(store)(App);
