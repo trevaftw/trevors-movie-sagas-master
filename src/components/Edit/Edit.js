6a0eedba054002fc8f3432081886b1d2663bcda8
Trevor Ramlow <trevorramlow@gmail.com>
2019-06-22 13:29:22 -0500

    add an input and text edit field to the edit component

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,9 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Edit extends Component {
-    render(){
-        return(
-            <h1>Edit</h1>
+    render() {
+        return (
+            <>
+                <h1>Edit</h1>
+                <input placeholder="New Movie Title"></input>
+                <br />
+                <textarea rows="4" cols="100"></textarea>
+            </>
         )
     }
 }
