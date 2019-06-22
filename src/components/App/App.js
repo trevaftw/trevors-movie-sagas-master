cda849d64fa329f14400a86762385181c18cc839
Trevor Ramlow <trevorramlow@gmail.com>
2019-06-22 13:17:44 -0500

    add the edit component and setup the link to it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Details from '../Details/Details';
 import Home from '../Home/Home';
+import Edit from '../Edit/Edit';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Route path="/details" component={Details} />
+          <Route path="/edit" component={Edit} />
           <Route exact path="/" component={Home} />
         </div>
       </Router>
--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,29 +1,33 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
-class Details extends Component{
+class Details extends Component {
 
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-            {this.props.reduxState.movies.map(movie => {
-                return (
-                    <h1>{movie.title}</h1>
-                )
-            })}
-            {this.props.reduxState.genres.map((genre, i) => {
-                return (
-                    <li key={i}>{genre.name}</li>
-                )
-            })}
+                {this.props.reduxState.movies.map((movie, i) => {
+                    return (
+                        <div key={i}>
+                            <h1>{movie.title}</h1>
+                            {this.props.reduxState.genres.map((genre, i) => {
+                                return (
+                                    <li key={i}>{genre.name}</li>
+                                )
+                            })}
+                            <p>{movie.description}</p>
+                        </div>
+                    )
+                })}
 
-            <Link to="/"><button>Return to Movie List</button></Link>
-            
-            {/* {JSON.stringify(this.props.reduxState.movies, null, 4)}
+
+                <Link to="/"><button>Return to Movie List</button></Link>
+                <Link to="/edit"><button>Modify the info</button></Link>
+                {/* {JSON.stringify(this.props.reduxState.movies, null, 4)}
             <br /> <br />
             {JSON.stringify(this.props.reduxState.genres, null, 4)} */}
             </>
@@ -33,6 +37,6 @@ class Details extends Component{
 
 const store = (reduxState) => ({
     reduxState
-  })
-  
+})
+
 export default connect(store)(Details);
